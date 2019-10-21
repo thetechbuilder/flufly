2058c7d67fdb9dcd67301d8daf9ad3deda1c37f5
Daniil Treshchin <dtreshchin@tesla.com>
2019-10-20 17:48:03 -0700

    Retab.

--- src/flufly_api/Services/ProductService.cs
+++ src/flufly_api/Services/ProductService.cs
@@ -11,21 +11,21 @@ using Newtonsoft.Json;
 namespace FNCPLT.API.Service
 {
     public class ProductService : BaseService, IProductService
-	{
-		private readonly IWebClientHelper _client;
-		private readonly IWebClientHelper _locationClient;
+    {
+        private readonly IWebClientHelper _client;
+        private readonly IWebClientHelper _locationClient;
 
-		public ProductService(
-			IWebClientLocator<string> locator, 
-			ILogger<IProductService> logger, 
-			ICacheService cache,
-			IOptions<ProcessAPIApplicationConfiguration> config = null) : base(locator, logger, config, cache)
-		{
-		    _client = locator.Get(Model.Enum.SERVICE_CLIENTS.FluflyProductService);
-		}
+        public ProductService(
+            IWebClientLocator<string> locator, 
+            ILogger<IProductService> logger, 
+            ICacheService cache,
+            IOptions<ProcessAPIApplicationConfiguration> config = null) : base(locator, logger, config, cache)
+        {
+            _client = locator.Get(Model.Enum.SERVICE_CLIENTS.FluflyProductService);
+        }
 
         public async Task<IServiceResponse<FluflyProductDTO>> GetFluflyProductsAsync(string productCategory, bool isActive) 
-		{
+        {
             string restResponse = null;
             try 
             {
@@ -37,27 +37,27 @@ namespace FNCPLT.API.Service
                 return null;
             }
 
-			var response = await ValidateResponse(restResponse);
-			var responseObject = JsonConvert.DeserealizeObject<ServiceResponse<IEnumerable<FluflyProductDTO>>>(response);
+            var response = await ValidateResponse(restResponse);
+            var responseObject = JsonConvert.DeserealizeObject<ServiceResponse<IEnumerable<FluflyProductDTO>>>(response);
 
-			if (responseObject != null && responseObject.Data.Count() != 0)
-			{
-				IList<FluflyProductDTO> fluflyDTO = AutoMapper.Mapper.Map<IEnumerable<FluflyProductDTO>>(responseObject.Data).toList();
-				if (fluflyDTO != null && fluflyDTO.Count > 0)
-				{
-					if (isActive == true)
-					{
-						for (int i = 0; i < fluflyDTO.Count; i++) {
-							if ((fluflyDTO[i].ProductEndDate == null || fluflyDTO[i].ProductEndDate >= DateTime.Now) && fluflyDTO[i].ProductStartDate >= DateTime.Now) {
-								fluflyDTO.Remove(fluflyDTO[i]);
-								i--;
-							}
-						}
-					}
-				}
-				return fluflyDTO;
-			}
-			return responseObject.Data;
-		}
+            if (responseObject != null && responseObject.Data.Count() != 0)
+            {
+                IList<FluflyProductDTO> fluflyDTO = AutoMapper.Mapper.Map<IEnumerable<FluflyProductDTO>>(responseObject.Data).toList();
+                if (fluflyDTO != null && fluflyDTO.Count > 0)
+                {
+                    if (isActive == true)
+                    {
+                        for (int i = 0; i < fluflyDTO.Count; i++) {
+                            if ((fluflyDTO[i].ProductEndDate == null || fluflyDTO[i].ProductEndDate >= DateTime.Now) && fluflyDTO[i].ProductStartDate >= DateTime.Now) {
+                                fluflyDTO.Remove(fluflyDTO[i]);
+                                i--;
+                            }
+                        }
+                    }
+                }
+                return fluflyDTO;
+            }
+            return responseObject.Data;
+        }
     }
 }
